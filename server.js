1f6c8e6843089d79c72a25d7221665c71c4a9e06
ry-oshiro <ry-oshiro@pdc.proto-g.co.jp>
2026-08-25 13:59:06 +0900

    名前修正機能・アプリ表示形式の変更

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// エンドポイントルーティング
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin', 'index.html'));
+});
+
+app.get('/admin/teams', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin', 'teams', 'index.html'));
+});
+
+app.get('/display', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'display', 'index.html'));
+});
+
+app.get('/judge', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'judge', 'index.html'));
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
